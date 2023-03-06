10c279ec1a3e746cb50326bf40dd26ed2cc22724
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-03-06 14:26:32 +0000

    Re-formatted printed timestamps for rate-limiting

--- get_repos.py
+++ get_repos.py
@@ -4,6 +4,7 @@ import os
 import requests
 from pprint import pprint
 from get_json import get_json
+from print_rate_limits import print_rate_limits
 
 USERNAME = 'cabinetoffice'
 
@@ -22,6 +23,7 @@ def get_repo_names(username):
         current_url = f"https://api.github.com/users/{username}/repos?page={page_number}&per_page={per_page}"
         print("")
         print(f"while_count - {while_count}")
+        print_rate_limits(current_url)
 
         headers, data = get_json(current_url)
         # breakpoint()
@@ -31,7 +33,7 @@ def get_repo_names(username):
 
         all_repo_names += [item['name'] for item in data]
 
-        print(all_repo_names)
+        # print(all_repo_names)
 
         # nextPattern = 'rel="next"'
         # link = get_next_page_link_request(current_url) # get string
@@ -47,7 +49,7 @@ def get_repo_names(username):
             print("Can't find next pattern, must be last page")
             pages_remaining = False
 
-    pprint(all_repo_names)
+    # pprint(all_repo_names)
 
     return all_repo_names
 
