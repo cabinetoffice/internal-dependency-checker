53a5cd771076f73a441b4d6de2f3cac9c52822ed
DanielMurray97 <daniel.murray@digital.cabinet-office.gov.uk>
2023-03-10 15:34:52 +0000

    extracting data from the CSV file now no longer ovverides the ORM

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from numpy import genfromtxt
 from sqlalchemy import create_engine, Column, String, Integer, Float
 # from sqlalchemy.ext.declarative import DeclarativeBase
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column
 import csv
 import pandas as pd
 
@@ -19,26 +19,24 @@ class Base(DeclarativeBase):
 class Report(Base):
     __tablename__ = "Report"
 
-    report_id = Column(Integer, primary_key=True)
-    dependency_name = Column(String)
-    description = Column(String)
-    vulnerability = Column(String)
-    severity_description = Column(String)
-    severity_score = Column(Float)
+    report_id: Mapped[int] = mapped_column(primary_key=True)
+    dependency_name: Mapped[str] = mapped_column(String(100))
+    description: Mapped[str] = mapped_column(String(1000))
+    vulnerability: Mapped[str] = mapped_column(String(1000))
+    severity_description: Mapped[str] = mapped_column(
+        String(1000), nullable=True)
+    severity_score: Mapped[float] = mapped_column(Float, nullable=True)
 
 
 if __name__ == '__main__':
+
     engine = create_engine('sqlite:///csv_report.db', echo=True)
-    # Base.metadata.create_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
 
     file_name = 'dependency-check-report.csv'
+
     df = pd.read_csv(file_name, usecols=[
-                     2, 4, 12, 17, 18], skiprows=1, header=None)
+                     2, 4, 12, 17, 18], names=['dependency_name', 'description', 'vulnerability', 'severity_description', 'severity_score'], header=0)
 
-    # specific_df = df[["DependencyName", "Description", "Vulnerability"]]
-
-    print(df)
-    # print(specific_df)
-
-    # df.to_sql(con=engine, index_label='id',
-    #           name=Report.__tablename__, if_exists='append')
+    df.to_sql(con=engine, index_label='report_id',
+              name=Report.__tablename__, if_exists='append')
