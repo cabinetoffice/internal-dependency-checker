c0d56328ec03da8a464836bfbd42315e0b03b590
Jim Narey <james.narey@digital.cabinet-office.gov.uk>
2023-03-17 13:12:43 +0000

    Generate report from downloaded dependency files

---
+++ generate_report.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+from sys import platform
+
+
+def dep_check(dir):
+    if platform == "linux":
+        print("is linux")
+        subprocess.run(
+            "/home/danny/Documents/dependency-check/bin/dependency-check.sh -f CSV --enableExperimental --scan . ", shell=True, cwd=dir)
+    elif platform == "darwin":
+        print("is mac")
+        subprocess.run(
+            "dependency-check --enableExperimental --scan .", shell=True, cwd=dir)
+
+
+def make_report(repos):
+
+    for root, subdir, files in os.walk(repos):
+
+        print(f'root: {root}')
+        print(f'subdirs: {subdir}')
+        print(f'files: {files}')
+
+        if 'package.json' in files and 'node_modules' not in files:
+            print(
+                f'Found a package.json file at directory: {root}. Npm installing and creating report...')
+
+            subprocess.run("npm install", shell=True, cwd=root)
+
+            dep_check(root)
+
+        if 'requirements.txt' in files:
+            print(
+                f'Found a requirements.txt file at directory: {root}. creating report...')
+            dep_check(root)
+
+
+if __name__ == "__main__":
+    repos = 'repos/'
+
+    make_report(repos)
