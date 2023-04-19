e8becc790e7c7a968baca5d2b05a3376cc32a22f
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-04-19 14:11:37 +0100

    added query_api fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -44,4 +44,16 @@ def url_with_changed_per_page_value():
 def url_with_query_string_to_be_added():
     return  f'https://example.com/search/code'
  
+# query_api() fixtures
 
+@pytest.fixture
+def header_no_link():
+    return  {'Server': 'GitHub.com', 'Date': 'Wed, 19 Apr 2023 10:40:07 GMT', 'Content-Type': 'application/json; charset=utf-8', 'Content-Length': '149'}
+
+@pytest.fixture
+def header_with_link():
+    return  {'Server': 'GitHub.com', 'Date': 'Wed, 19 Apr 2023 10:40:07 GMT', 'Content-Type': 'application/json; charset=utf-8', 'Content-Length': '149', 'link': 'page=2'}
+
+@pytest.fixture
+def json_():
+    return  {'message': 'Validation Failed', 'errors': [{'resource': 'Search', 'field': 'q', 'code': 'missing'}], 'documentation_url': 'https://docs.github.com/v3/search'}
--- tests/test_query_api.py
+++ tests/test_query_api.py
@@ -1,18 +1,12 @@
 import pytest
 import query_api
 
-header_no_link = {'Server': 'GitHub.com', 'Date': 'Wed, 19 Apr 2023 10:40:07 GMT', 'Content-Type': 'application/json; charset=utf-8', 'Content-Length': '149'}
-
-header_with_link = {'Server': 'GitHub.com', 'Date': 'Wed, 19 Apr 2023 10:40:07 GMT', 'Content-Type': 'application/json; charset=utf-8', 'Content-Length': '149', 'link': 'page=2'}
-
-json_ = {'message': 'Validation Failed', 'errors': [{'resource': 'Search', 'field': 'q', 'code': 'missing'}], 'documentation_url': 'https://docs.github.com/v3/search'}
-
-def test_query_api(mocker):
+def test_query_api(mocker, header_no_link, json_):
     mocker.patch('query_api.get_json', return_value=(header_no_link, json_))
     pages = query_api.query_api('url', 'key')
     assert ' '.join([str(page) for page in pages]).count('message') == 1
 
-def test_query_api_multiple_pages(mocker):
+def test_query_api_multiple_pages(mocker, header_with_link, json_):
     mocker.patch('query_api.get_json', return_value=(header_with_link, json_))
     pages = query_api.query_api('url', 'key')
     assert ' '.join([str(page) for page in pages]).count('message') == 2
