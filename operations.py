53b49dc73ef0e03147b8fec7ee4ebda9ecbc6a76
DanielMurray97 <daniel.murray@digital.cabinet-office.gov.uk>
2023-05-09 16:02:32 +0100

    added write dependencies to file in operations

--- operations.py
+++ operations.py
@@ -1,7 +1,10 @@
 from pprint import pprint
+import os
 
+from dependency_data import dependency_data
 import query_api
 import db
+import write_dependencies_to_file as wd
 
 # Runners
 
@@ -11,6 +14,11 @@ def _get_repos(cli_args):
 
 def _get_dep_files(cli_args):
     all_deps = query_api.get_dep_files(cli_args['github-username'], cli_args['repo-name'], cli_args['dep-file'], cli_args['github-key'])
+
+    print("Writing to file...")
+
+    wd.write_dependencies_to_file(cli_args['repo-name'], cli_args['github-key'], all_deps)
+
     pprint(all_deps)
 
 def _filter_reports(cli_args):
