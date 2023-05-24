ec28444fbcaefe4424a3a5b930eb8c3b7793b418
Mouhajer-CO <mouhajer.zaouali@digital.cabinet-office.gov.uk>
2023-05-24 14:43:48 +0100

    Add ORGANIZATION and GITHUB_KEY checks before running init script

--- src/main.js
+++ src/main.js
@@ -1,38 +1,13 @@
-import {
-    getOrganizationRepos,
-    checkFileExists,
-    getInfoFromOrganizationRepos,
-    updateStateDependencies,
-    getTechFile,
-    writeDependencyFiles
-} from "./utils.js";
+import { init } from "./init";
+import { ORGANIZATION, GITHUB_KEY } from "./config.js";
 
-import {
-    DEPENDENCY_FILES
-} from "./config.js";
-
-// import { repos } from "../test/mock/repos.js";
-
-const init = async ( organization ) => {
-    try {
-        const repos = await getOrganizationRepos(organization);
-        for ( const repo of repos ) {
-            const { repoName, org, branch, filter, fileUrl } = getInfoFromOrganizationRepos( repo );
-            for ( const fileName of DEPENDENCY_FILES[filter] ) {
-                if( await checkFileExists(`${fileUrl}/${fileName}`) ) {
-                    const { tech, key } = getTechFile(fileName);
-                    updateStateDependencies(tech, key, repoName, org, branch, `${fileUrl}/${fileName}`)
-                }
-            }
-        }
-        console.log( `Creation of the state file completed.` );
-
-        // Write dependency states to file on the infrastructure/dependencies folder.
-        // Files needed on the container when docker compose up is triggered (It is going to be fun :))
-        writeDependencyFiles();
-    } catch (error) {
-        console.error( `init: error ${error.message}` );
+if (ORGANIZATION && GITHUB_KEY) {
+    init(ORGANIZATION);
+} else {
+    if (!ORGANIZATION) {
+        console.error(`GitHub organization is missing`);
+    }
+    if (!GITHUB_KEY) {
+        console.error(`Access Token/Github key is missing`);
     }
 }
-
-init(process.argv[2]);
