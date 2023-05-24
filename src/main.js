5a19fc4f42c5c2f64a7fb8ca84c18d7c6629a661
Mouhajer-CO <mouhajer.zaouali@digital.cabinet-office.gov.uk>
2023-05-24 15:06:39 +0100

    Update README and fix main

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import { init } from "./init";
+import { init } from "./init.js";
 import { ORGANIZATION, GITHUB_KEY } from "./config.js";
 
 if (ORGANIZATION && GITHUB_KEY) {
-    init(ORGANIZATION);
+    init();
 } else {
     if (!ORGANIZATION) {
         console.error(`GitHub organization is missing`);
