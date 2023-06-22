0888291aa820689eab9fa14eb50e190b2fe5ff29
Mouhajer-CO <mouhajer.zaouali@digital.cabinet-office.gov.uk>
2023-06-22 13:47:23 +0100

    Add GO lang dependencies checks and enable it on compose file

--- infrastructure/dependencies/go/dep-checker.sh
+++ infrastructure/dependencies/go/dep-checker.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-REPORTS_FOLDER_NAME=reports
+REPORTS_FOLDER_NAME=reports/go
+WORKDIR=/repo-dependency-checker
 
 mkdir -p $REPORTS_FOLDER_NAME
 
-# Read the dependencies from the JSON file
-dependencies=$(/usr/bin/jq -c '.dependencies[]' ./dependencies.json)
+dependencies=$(/usr/bin/jq -c '.go[] | {file_name: .file_name, repo_file_path: .repo_file_path}' ./repos/state.json)
 
 # Loop over the dependencies
 for dependency in $dependencies
 do
+  # Extract the dependency arguments using jq
+  file_name=$(echo "$dependency" | jq -r '.file_name')
+  repo_file_path=$(echo "$dependency" | jq -r '.repo_file_path')
 
   TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
-
-  # Extract the dependency arguments using jq
-  file1=$(echo "$dependency" | jq -r '.file1')
-  file_name=$(echo "$dependency" | jq -r '.file_name')
+  REPORT_FILE_NAME=${WORKDIR}"/"${REPORTS_FOLDER_NAME}"/"$file_name"__gosec__"$TIMESTAMP".json"
 
   # Print arguments
-  echo "Contents: $file1 $file_name and $TIMESTAMP"
-  echo "TBD"
+  echo "Contents: $file_name, $repo_file_path and $TIMESTAMP"
+  cd $WORKDIR"/"$repo_file_path
+  gosec -fmt=json -out=$REPORT_FILE_NAME ./...
+  echo "Saved report file to $REPORT_FILE_NAME"
 done
