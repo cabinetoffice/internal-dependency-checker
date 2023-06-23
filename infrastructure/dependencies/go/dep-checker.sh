cb65d2d85ed1edbff657daff8c3f14110359a17d
Mouhajer-CO <mouhajer.zaouali@digital.cabinet-office.gov.uk>
2023-06-23 13:22:24 +0100

    Add nancy checks for golang dependencies

--- infrastructure/dependencies/go/dep-checker.sh
+++ infrastructure/dependencies/go/dep-checker.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-REPORTS_FOLDER_NAME=reports/go
+NANCY_REPORTS_FOLDER_NAME=reports/go/nancy
+GOSEC_REPORTS_FOLDER_NAME=reports/go/gosec
 WORKDIR=/repo-dependency-checker
 
-mkdir -p $REPORTS_FOLDER_NAME
+mkdir -p $NANCY_REPORTS_FOLDER_NAME
+mkdir -p $GOSEC_REPORTS_FOLDER_NAME
 
 dependencies=$(/usr/bin/jq -c '.go[] | {file_name: .file_name, repo_file_path: .repo_file_path}' ./repos/state.json)
 
@@ -15,11 +17,16 @@ do
   repo_file_path=$(echo "$dependency" | jq -r '.repo_file_path')
 
   TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
-  REPORT_FILE_NAME=${WORKDIR}"/"${REPORTS_FOLDER_NAME}"/"$file_name"__gosec__"$TIMESTAMP".json"
+  NANCY_REPORT_FILE_NAME=${WORKDIR}"/"${NANCY_REPORTS_FOLDER_NAME}"/"$file_name"__nancy__"$TIMESTAMP".json"
+  GOSEC_REPORT_FILE_NAME=${WORKDIR}"/"${GOSEC_REPORTS_FOLDER_NAME}"/"$file_name"__gosec__"$TIMESTAMP".json"
 
   # Print arguments
   echo "Contents: $file_name, $repo_file_path and $TIMESTAMP"
+
   cd $WORKDIR"/"$repo_file_path
-  gosec -fmt=json -out=$REPORT_FILE_NAME ./...
-  echo "Saved report file to $REPORT_FILE_NAME"
+  gosec -fmt=json -out=$GOSEC_REPORT_FILE_NAME ./...
+  echo "Saved gosec report file to $GOSEC_REPORT_FILE_NAME"
+  go list -json -deps ./... | nancy sleuth --output=json > $NANCY_REPORT_FILE_NAME
+  echo "Saved nancy report file to $NANCY_REPORT_FILE_NAME"
+
 done
