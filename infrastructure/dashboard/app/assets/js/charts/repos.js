5624e77fb4dac7885201196bb773598b551efc17
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-07-26 14:19:51 +0100

    Consolidate repo data visualisations:
        - Remove date updated, language and fork data from repo tables
        - Remove piecharts and bar chart from repo charts
        - Update repo table and chart html

--- infrastructure/dashboard/app/assets/js/charts/repos.js
+++ infrastructure/dashboard/app/assets/js/charts/repos.js
@@ -1,110 +1,3 @@
-const createRepoPiecharts = (repos) => {
-
-    const reposForkedData = { forked: 0, not_forked: 0 };
-    const reposArchivedData = { archived: 0, not_archived: 0 };
-    const reposVisibilityData = { public: 0, private: 0 };
-
-    repos.forEach(repo => {
-        reposForkedData[repo.fork ? 'forked' : 'not_forked']++;
-        reposArchivedData[repo.archived ? 'archived' : 'not_archived']++;
-        reposVisibilityData[repo.visibility === 'public' ? 'public' : 'private']++;
-    });
-
-    const reposForkedCtx = document.getElementById('repos-forked-chart').getContext('2d');
-    const reposArchivedCtx = document.getElementById('repos-archived-chart').getContext('2d');
-    const reposVisibilityCtx = document.getElementById('repos-visibility-chart').getContext('2d');
-
-    const reposForkedChart = new Chart(reposForkedCtx, {
-        type: 'pie',
-        data: {
-            labels: ["Forked", "Not Forked"],
-            datasets: [{
-                data: Object.values(reposForkedData),
-                backgroundColor: ['blue', 'gray'],
-            }]
-        },
-        options: {
-            responsive: false
-        }
-    });
-
-    const reposArchivedChart = new Chart(reposArchivedCtx, {
-        type: 'pie',
-        data: {
-            labels: ["Archived", "Not Archived"],
-            datasets: [{
-                data: Object.values(reposArchivedData),
-                backgroundColor: ['orange', 'gray'],
-            }]
-        },
-        options: {
-            responsive: false
-        }
-    });
-
-    const reposVisibilityChart = new Chart(reposVisibilityCtx, {
-        type: 'pie',
-        data: {
-            labels: ["Public", "Private"],
-            datasets: [{
-                data: Object.values(reposVisibilityData),
-                backgroundColor: ['green', 'red'],
-            }]
-        },
-        options: {
-            responsive: false
-        }
-    });
-
-}
-
-const languageBarChartOptions = {
-    scales: {
-        x: {
-            display: true,
-            title: {
-                display: true,
-                text: 'Main languages of Repositories'
-            }
-        },
-        y: {
-            display: true,
-            title: {
-                display: true,
-                text: 'Number of Repositories'
-            }
-        }
-    }
-};
-
-const createRepoLanguageBarChart = (repos) => {
-
-    const languagesData = repos.reduce((acc, repo) => {
-        if (repo.language === null) {
-            acc['No Main Language'] = acc['No Main Language'] + 1 || 1;
-        } else {
-            acc[repo.language] = acc[repo.language] + 1 || 1;
-        }
-        return acc;
-    }, {});
-
-    const languageCtx = document.getElementById('repos-language-chart').getContext('2d');
-
-    const languageBarChart = new Chart(languageCtx, {
-        type: "bar",
-        data: {
-            labels: Object.keys(languagesData),
-            datasets: [{
-                backgroundColor: 'rgb(75, 192, 192)',
-                label: '# of Repositories',
-                data: Object.values(languagesData)
-            }]
-        },
-        options: languageBarChartOptions
-    });
-
-}
-
 const createRepoScatterChart = (repos) => {
 
     const repoScatterChartData = repos.map(repo => ({
@@ -117,7 +10,7 @@ const createRepoScatterChart = (repos) => {
         type: 'scatter',
         data: {
             datasets: [{
-                label: 'Date created vs updated',
+                label: 'Date created vs last updated',
                 data: repoScatterChartData,
                 borderColor: 'red',
                 backgroundColor: 'red',
@@ -127,7 +20,10 @@ const createRepoScatterChart = (repos) => {
             plugins: {
                 title: {
                     display: true,
-                    text: 'Dates of Repositories created vs updated',
+                    text: 'Dates of Repositories created vs last updated',
+                    font: {
+                        size: 24
+                    },
                 },
                 tooltip: {
                     mode: 'index',
@@ -137,7 +33,7 @@ const createRepoScatterChart = (repos) => {
                             return `Repository: ${repoName}`;
                         }
                     }
-                }
+                },
             },
             scales: {
                 x: {
@@ -151,7 +47,10 @@ const createRepoScatterChart = (repos) => {
                     },
                     title: {
                         display: true,
-                        text: 'Date Repository Created',
+                        text: 'Date created',
+                        font: {
+                            size: 18
+                        }
                     }
                 },
                 y: {
@@ -165,7 +64,10 @@ const createRepoScatterChart = (repos) => {
                     },
                     title: {
                         display: true,
-                        text: 'Date Repository Updated',
+                        text: 'Date last updated',
+                        font: {
+                            size: 16
+                        }
                     }
                 }
             },
@@ -179,7 +81,5 @@ const createRepoScatterChart = (repos) => {
 (async () => {
     const reposData = await getRepos();
     const reposChartData = reposData.map(extractRepoVisualisationData);
-    createRepoPiecharts(reposChartData);
-    createRepoLanguageBarChart(reposChartData);
     createRepoScatterChart(reposChartData);
 })()
--- infrastructure/dashboard/app/assets/js/table/repos_table.js
+++ infrastructure/dashboard/app/assets/js/table/repos_table.js
@@ -4,19 +4,15 @@ const setReposTableContent = (template, tbodyEl, visualisationData) => {
     const a = clone.querySelector("a");
 
     a.textContent = visualisationData.name;
-    a.href = visualisationData.htmlUrl;
+    a.href = visualisationData.html_url;
 
-    td[1].textContent = visualisationData.description ? visualisationData.description : "No description provided";
+    td[1].textContent = visualisationData.description ? visualisationData.description : "N/A";
     td[2].textContent = new Date(visualisationData.created_at).getFullYear();
-    td[3].textContent = new Date(visualisationData.updated_at).getFullYear();
-    td[4].textContent = visualisationData.language ? visualisationData.language : "No main language";
-    td[5].textContent = visualisationData.fork ? 'Yes' : "No";
-    td[6].textContent = visualisationData.archived ? "Yes" : "No";
-    td[7].textContent = visualisationData.visibility;
+    td[3].textContent = visualisationData.archived ? "Yes" : "No";
+    td[4].textContent = visualisationData.visibility;
 
     tbodyEl.appendChild(clone);
 };
-
 const updateReposTable = (repos) => {
     const tbodyEl = document.querySelector("tbody");
     while (tbodyEl.rows.length > 0) {
