be96edef62f53377f1430fefba5b1bcb7d520537
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-12-19 12:14:06 +0000

    replace all console.log/error calls in exec.ts with logger methods and update test suite

--- test/src/utils/exec.spec.ts
+++ test/src/utils/exec.spec.ts
@@ -1,16 +1,20 @@
-import { describe, expect, test, jest, afterEach, beforeEach } from '@jest/globals';
+jest.mock('../../../src/utils/logger', () => ({
+    log: {
+        info: jest.fn(),
+        error: jest.fn()
+    }
+}));
+
+import { describe, expect, test, jest, afterEach } from '@jest/globals';
 
 import { exec_command } from '../../../src/utils/exec';
 
-const spyConsoleLog = jest.spyOn(console, 'log');
-const spyConsoleError = jest.spyOn(console, 'error');
+import { log } from "../../../src/utils/logger";
+
+const mockLogError = log.error as jest.Mock;
+const mockLogInfo = log.info as jest.Mock;
 
 describe("UTILS exec tests suites", () => {
-
-    beforeEach(() => {
-        spyConsoleLog.mockImplementation(() => {/**/});
-        spyConsoleError.mockImplementation(() => {/**/});
-    });
 
     afterEach(() => {
         jest.resetAllMocks();
@@ -19,11 +23,11 @@ describe("UTILS exec tests suites", () => {
     test('should call execPromise and print logs', async () => {
         await exec_command("ls", 5, 99);
 
-        expect(spyConsoleLog).toHaveBeenCalledTimes(3);
-        expect(spyConsoleLog).toHaveBeenCalledWith(`5/99 - execute command: ls`);
-        expect(spyConsoleLog).toHaveBeenCalledWith("stdout:", expect.anything());
-        expect(spyConsoleLog).toHaveBeenCalledWith("stderr:", expect.anything());
-        expect(spyConsoleError).toHaveBeenCalledTimes(0);
+        expect(mockLogInfo).toHaveBeenCalledTimes(3);
+        expect(mockLogInfo).toHaveBeenCalledWith(`5/99 - execute command: ls`);
+        expect(mockLogInfo).toHaveBeenCalledWith(expect.stringContaining("stdout: "));
+        expect(mockLogInfo).toHaveBeenCalledWith(expect.stringContaining("stderr: "));
+        expect(mockLogError).toHaveBeenCalledTimes(0);
     });
 
 });
